3ff12f8a3125056258e823f4e4ecdba369d95fd5
antonyvg89 <antovellookunnel@gmail.com>
2022-03-29 16:54:14 +0530

    adfd

---
+++ Test25.java
@@ -0,0 +1,19 @@
+class Test25
+{
+	public static void main(String args[])
+	{
+		int a=Integer.parseInt(args[0]);
+		//int b=Integer.parseInt(args[1]);
+		int c=a%2;
+		if(c==1)
+		{
+			System.out.println("odd number");
+		}
+		else
+		{
+			System.out.println("even number");
+		}
+	}
+	
+	
+}
